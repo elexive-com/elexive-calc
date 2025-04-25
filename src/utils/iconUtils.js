465d6ad6cf91407793e0c1529f7802eea0f607df
Rolf Koski <rolle@iki.fi>
2025-04-25 13:38:11 +0300

    removed unnecesaary fontaweome import

--- src/utils/iconUtils.js
+++ src/utils/iconUtils.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
   faChartLine, faBullseye, faPuzzlePiece, 
   faMoneyBillWave, faGears, faCheck, faBuilding, 
